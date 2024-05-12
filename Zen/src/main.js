3df3875073881423140258975844c175e73bd96e
Matthew Zenn <spyguy57@gmail.com>
2024-05-12 15:48:48 -0400

    Added debug console

--- Zen/src/main.js
+++ Zen/src/main.js
@@ -2,17 +2,27 @@ const { invoke } = window.__TAURI__.core;
 const editor = document.getElementById('editor');
 const input = document.getElementById('input');
 const numbers = document.getElementById("numbers");
+const consol = document.getElementById('consol');
 
+var check = 0;
+var errs = [];
+
+console.log = function(message) {
+  consol.value = message+"\n";
+};
 
 document.getElementById('send').addEventListener('click', () => {
   if (input.value != '') {
     if (input.value == "cls" | input.value == "Cls") {
+      document.getElementById("status").style.color = "#30363d";
       editor.value = '';
       input.value = '';
+      consol.value = '';
     }
     else {
       editor.value += input.value+'\n';
       input.value = '';
+      input.focus();
     }
   }
   else {
@@ -24,8 +34,10 @@ input.addEventListener("keydown", (ev) => {
   if (ev.key === "Enter") {
     if (input.value != '') {
       if (input.value == "cls" | input.value == "Cls") {
+        document.getElementById("status").style.color = "#30363d";
         editor.value = '';
         input.value = '';
+        consol.value = '';
       }
       else {
         editor.value += input.value+'\n';
@@ -56,19 +68,38 @@ editor.addEventListener("keydown", (event) => {
 });
 
 document.getElementById('run').addEventListener('click', function() {
+  consol.value = '';
+  errs = [];
   document.getElementById("status").style.color = "#30363d";
   window.addEventListener("error", errorlog);
   eval(editor.value);
 
+  if(consol.value != '') {
+    document.getElementById("status").style.color = "#28ff82";
+
+  }
+
   function errorlog(Error) {
     if (Error) {
       errs.push(Error);
-      //document.getElementById("logger").value += Error.message + '\n';
-      //document.getElementById("error").innerHTML = '<span id="status"></span> ' + errs.length + ' Errors';
+      document.getElementById("consol").value += Error.message + '\n';
       document.getElementById("status").style.color = "#FF5A5A";
     }
     else {
       return;
     }
+  }
+});
+
+document.getElementById('dbg').addEventListener('click', () => {
+  if (check == 0) {
+    consol.style.display = 'block';
+    document.getElementById('dbg').style.color = "#9e74ff";
+    check = 1;
+  }
+  else {
+    consol.style.display = 'none';
+    document.getElementById('dbg').style.color = "#848d97";
+    check = 0;
   }
 });
